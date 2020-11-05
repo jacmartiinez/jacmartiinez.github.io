e47497f94c3f6e2d678e2eb17694f4af562d8f36
Jacqueline <jacque.emartinez@hotmail.com>
2020-11-05 10:32:29 -0500

    home

--- project2/interiordesign_dorothydraper/assets/toggle.js
+++ project2/interiordesign_dorothydraper/assets/toggle.js
@@ -1,8 +1,19 @@
-$(document).ready(function (){
-    //ID or Class name of the button goes into the first section
-    $('#button').click(function() {
-        //ID or Class name of the modal you want to appear on click goes into this second area.
-        $("#button").toggleClass("visible");
-        $("#button-hover").toggleClass("invisible");
-        });
-});
+//Get the button:
+mybutton = document.getElementById("button");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
